8e0d8f8f10ac8d5bae263b3b972732ab96ea2953
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2021-06-09 08:34:45 +0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,10 +22,15 @@ module.exports = {
     // Language of this theme. See the [Theme Language] section below.
     lang: 'en-US',
 
+   lang: Object.assign(require('vuepress-theme-meteorlxy/lib/langs/en-US'), {
+      home: 'Welcome to Musik Bot Blog',
+      posts: 'My Post',
+    }),
+
     // Personal infomation (delete the fields if you don't have / don't want to display)
     personalInfo: {
       // Nickname
-      nickname: 'meteorlxy',
+      nickname: 'bayu',
 
       // Introduction of yourself (HTML supported)
       description: 'Random Person',
@@ -49,92 +54,14 @@ module.exports = {
       sns: {
         // Github account and link
         github: {
-          account: 'meteorlxy',
-          link: 'https://github.com/meteorlxy',
-        },
-
-        // Facebook account and link
-        facebook: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.facebook.com/meteorlxy.cn',
-        },
-
-        // LinkedIn account and link
-        linkedin: {
-          account: 'meteorlxy',
-          link: 'http://www.linkedin.com/in/meteorlxy',
+          account: 'BayuBatam2008',
+          link: 'https://github.com/BayuBatam2008',
         },
 
         // Twitter account and link
         twitter: {
-          account: 'meteorlxy_cn',
-          link: 'https://twitter.com/meteorlxy_cn',
-        },
-
-        // Sina Weibo account and link
-        weibo: {
-          account: '@焦炭君_Meteor',
-          link: 'https://weibo.com/u/2039655434',
-        },
-
-        // Zhihu account and link
-        zhihu: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.zhihu.com/people/meteorlxy.cn',
-        },
-
-        // Douban account and link
-        douban: {
-          account: '159342708',
-          link: 'https://www.douban.com/people/159342708',
-        },
-
-        // Reddit account and link
-        reddit: {
-          account: 'meteorlxy',
-          link: 'https://www.reddit.com/user/meteorlxy',
-        },
-
-        // Medium account and link
-        medium: {
-          account: 'meteorlxy.cn',
-          link: 'https://medium.com/@meteorlxy.cn',
-        },
-
-        // Instagram account and link
-        instagram: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.instagram.com/meteorlxy.cn',
-        },
-
-        // GitLab account and link
-        gitlab: {
-          account: 'meteorlxy',
-          link: 'https://gitlab.com/meteorlxy',
-        },
-
-        // Bitbucket account and link
-        bitbucket: {
-          account: 'meteorlxy',
-          link: 'https://bitbucket.org/meteorlxy',
-        },
-
-        // Docker Hub account and link
-        docker: {
-          account: 'meteorlxy',
-          link: 'https://hub.docker.com/u/meteorlxy',
-        },
-
-        // CSDN account and link
-        csdn: {
-          account: '',
-          link: 'https://www.csdn.net/',
-        },
-
-        // Juejin account and link
-        juejin: {
-          account: 'meteorlxy',
-          link: 'https://juejin.im/user/5c6fa9dde51d453fcb7baf09',
+          account: 'BayuBatam2008',
+          link: 'https://twitter.com/BayuBatam2008',
         },
       },
     },
@@ -157,13 +84,13 @@ module.exports = {
     // Footer Config (Optional)
     footer: {
       // Show 'Powered by VuePress' or not (enable it to support VuePress)
-      poweredBy: true,
+      poweredBy: false,
 
       // Show the theme that you are using (enable it to support this theme) (please do not disable it, haha)
-      poweredByTheme: true,
+      poweredByTheme: false,
 
       // Add your custom footer (HTML supported)
-      custom: 'Copyright 2018-present <a href="https://github.com/meteorlxy" target="_blank">meteorlxy</a> | MIT License',
+      custom: 'Made By Bayu With ❤️',
     },
 
     // Info Card Config (Optional)
@@ -197,8 +124,8 @@ module.exports = {
 
     // Comments config. See the [Posts Comments] section below.
     comments: {
-      owner: 'meteorlxy',
-      repo: 'vuepress-theme-meteorlxy',
+      owner: 'BayuBatam2008',
+      repo: 'mskbotblog',
       clientId: 'MY_CLIENT_ID',
       clientSecret: 'MY_CLIENT_SECRET',
     },
